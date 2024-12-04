ddf6528bbf9ff43bc83230ae85153e42c130f870
Riley <waskerdu@gmail.com>
2024-12-03 22:37:37 -0800

    finally did 2024 day 3 part 2

--- 2024/d03.py
+++ 2024/d03.py
@@ -28,41 +28,62 @@ def parse(txt: str):
         t += int(x)*int(y)
     print(t)
 
-def match(txt: str, idx: int):
-    ss = ['mul(', 'do()', 'don\'t()']
-    id = 0
-    for s in ss:
-        f = txt.find(s, idx)
-        if f == -1:
-            continue
-    
-
 def parse2(txt: str):
-    idx = 0
-    t = 0
-    enable = True
-    while True:
-        idx = txt.find('mul(', idx)
-        # print(txt[idx:])
-        if idx == -1:
-            break
-        idx += 4
-        comma = txt.find(',', idx)
+    start = 0
+    current = 0
+    total = 0
+    enabled = True
+    def at_eof() -> bool:
+        return current >= len(txt)
+    def peek() -> str:
+        return txt[current]
+    def advance() -> str:
+        nonlocal current
+        v = peek()
+        current += 1
+        return v
+    def match(word: str) -> bool:
+        nonlocal current
+        for c in word:
+            if c != peek():
+                current = start
+                return False
+            advance()
+        return True
+    def mul() -> bool:
+        nonlocal total
+        if not match('mul('):
+            return False
+        comma = txt.find(',', current)
         if comma == -1:
-            continue
-        x = txt[idx:comma]
+            return False
+        x = txt[current:comma]
         if not x.isdecimal():
-            continue
-        idx = comma + 1
-        paren = txt.find(')', idx)
+            return False
+        # print('mul', x)
+        paren = txt.find(')', comma)
         if paren == -1:
-            continue
-        y = txt[idx:paren]
+            return False
+        y = txt[comma+1:paren]
         if not y.isdecimal():
-            continue
-        if enable:
-            t += int(x)*int(y)
-    print(t)
+            return False
+        print(f'mul({x}, {y})')
+        if enabled:
+            total += int(x)*int(y)
+        return True
+    while not at_eof():
+        start = current
+        if match('do()'):
+            enabled = True
+            print('do()')
+        elif match('don\'t()'):
+            enabled = False
+            print('don\'t()')
+        elif mul():
+            pass
+        else:
+            advance()
+    print('total:', total)
 
 def pt1():
     with open('./2024/d03.txt') as f:
@@ -74,5 +95,5 @@ def pt2():
         line = f.read()
         parse2(line)
 
-pt1()
-# pt2()
+# pt1()
+pt2()
